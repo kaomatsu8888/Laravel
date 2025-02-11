7f97985dfe63842942119a6d3589211b11599d07
kaomatsu8888 <m.kaoru0830@gmail.com>
2025-02-11 20:10:27 +0900

    ルーティングチェック

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// ローカルホスト=↓の/ ＞routes/welcome.blade.phpを表示
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/route/hello', function () {
+    return '<h1>Hello from Route!</h1>';
+});
+
+Route::get('/view/hello', function () {
+    return view('message.hello');
 });
