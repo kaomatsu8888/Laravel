385e779377533cbdce3ce474ff54cf680709ee5c
kaomatsu8888 <m.kaoru0830@gmail.com>
2025-02-13 21:26:49 +0900

    パラメータ

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,16 @@ Route::get('/view/hello', function () {
 Route::get('/view/var', function () {
     return view('message.var', ['variable' => 'Hello from web.php']);
 });
-
+// 変数を無名関数に渡す
+//msgに変数を渡す。/view/word/(URLの上にいます)とカッコ部分をいれるとURL表示が
+//http://localhost/view/word/URLの上にいますとなる
 Route::get('/view/word/{msg}', function ($msg) {
     return view('message.word', ['msg' => $msg]);
+});
+// ビューを呼び出す(複数パラメータを渡す)
+Route::get('/view/word/{name}/{msg}', function($name, $msg) {
+    return view('message.word2', [
+        'name' => $name, 
+        'msg' => $msg
+    ]);
 });
